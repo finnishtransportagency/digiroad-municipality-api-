4374e861c8d58b6f7a9e85acb245de7b70c8fd8c
MarkusRegardh <markus.regardh@gmail.com>
2022-07-07 10:34:26 +0300

    added VPC

--- serverless.ts
+++ serverless.ts
@@ -18,6 +18,13 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    vpc: {
+      securityGroupIds: [process.env.SECURITYGROUPID],
+      subnetIds: [
+        process.env.SUBNETAID,
+        process.env.SUBNETBID
+      ]
+    }
   },
   // import the function via paths
   functions: { hello, importData },
