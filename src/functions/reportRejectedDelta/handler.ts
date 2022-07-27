b842c5a401c6ad85c12a60dd4aab6e1135eeb317
HenrikSundell <henrik.sundell@gmail.com>
2022-07-27 17:42:08 +0300

    Add report rejected lambda

--- src/functions/reportRejectedDelta/handler.ts
+++ src/functions/reportRejectedDelta/handler.ts
@@ -1,9 +1,18 @@
 import { middyfy } from '@libs/lambda';
+import * as aws from 'aws-sdk';
 
 const reportRejectedDelta = async (event) => {
-  console.log(`reportRejectedDelta invoked. Event: ${JSON.stringify(event)}`);
+  const s3 = new aws.S3();
+  const now = new Date().toISOString().slice(0, 19);
+  const municipality = 'espoo';
 
-  return JSON.stringify('hello from reject');
+  const params = {
+    Bucket: `dr-kunta-${process.env.STAGE_NAME}-bucket`,
+    Key: `logs/${municipality}/${now}`,
+    Body: JSON.stringify(event)
+  };
+
+  s3.upload(params);
 };
 
 export const main = middyfy(reportRejectedDelta);
