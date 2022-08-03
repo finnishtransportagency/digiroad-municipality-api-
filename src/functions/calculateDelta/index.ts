f8c3393015cd3f93a2eddd6b2774d4d1fea89e81
MarkusRegardh <markus.regardh@gmail.com>
2022-08-03 12:51:48 +0300

    Add maximumRetryAttempts
    Add a maximumRetryAttempts to calculateDelta to stop it from retrying

--- src/functions/calculateDelta/index.ts
+++ src/functions/calculateDelta/index.ts
@@ -2,6 +2,7 @@ import { handlerPath } from '@libs/handler-resolver';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
+  maximumRetryAttempts: 0,
   events: [
     {
       s3: {
