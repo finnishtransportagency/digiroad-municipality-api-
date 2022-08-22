8cf37b207008a684bd010f0c1b2fe55af74cf00d
HenrikSundell <henrik.sundell@gmail.com>
2022-08-22 15:14:20 +0300

    Revert "Remove security group"
    This reverts commit 5164259a7530f7146c82df33cfff8c54b350d97c.

--- src/functions/getNearbyLinks/index.ts
+++ src/functions/getNearbyLinks/index.ts
@@ -3,6 +3,7 @@ import { handlerPath } from '@libs/handler-resolver';
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   vpc: {
+    securityGroupIds: [process.env.DIGIROADSECURITYGROUPID],
     subnetIds: [process.env.DIGIROADSUBNETAID, process.env.DIGIROADSUBNETBID]
   },
   environment: {
