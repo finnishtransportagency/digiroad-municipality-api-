5164259a7530f7146c82df33cfff8c54b350d97c
HenrikSundell <henrik.sundell@gmail.com>
2022-08-22 14:53:16 +0300

    Remove security group

--- src/functions/getNearbyLinks/index.ts
+++ src/functions/getNearbyLinks/index.ts
@@ -3,7 +3,6 @@ import { handlerPath } from '@libs/handler-resolver';
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   vpc: {
-    securityGroupIds: [process.env.DIGIROADSECURITYGROUPID],
     subnetIds: [process.env.DIGIROADSUBNETAID, process.env.DIGIROADSUBNETBID]
   },
   environment: {
