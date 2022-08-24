092ec0bcd9433b53204075ff319ef3db508b72e7
MarkusRegardh <markus.regardh@gmail.com>
2022-08-24 12:56:04 +0300

    Implement getRoadLinks lambda

--- src/functions/getNearbyLinks/handler.ts
+++ src/functions/getNearbyLinks/handler.ts
@@ -1,8 +1,67 @@
 import { middyfy } from '@libs/lambda';
+import { Client } from 'pg';
+import { parse } from 'wellknown';
+import { LineString4D, LinkPoint } from '@functions/typing';
 
-const storeMunicipalityData = async (event) => {
-  console.log(JSON.stringify(event));
-  return;
+const gerNearbyLinks = async (event) => {
+  const client = new Client({
+    host: process.env.PGHOST,
+    port: parseInt(process.env.PGPORT || '5432'),
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD
+  });
+  await client.connect();
+  const params = {
+    features: event
+  };
+
+  const query = {
+    text: `
+    SELECT (value#>'{properties}'->>'ID')::DECIMAL AS ID, json_agg((st_astext(shape),linkid)) AS roadlinks
+    FROM json_array_elements(($1)::json->'features') AS features, roadlink
+    WHERE ST_BUFFER(ST_SETSRID(ST_GeomFromGeoJSON(features->>'geometry'), 3067), 20) && roadlink.shape
+    GROUP BY ID
+    `,
+    values: [params]
+  };
+  return client
+    .query(query)
+    .then((res) => {
+      res.rows.forEach((row) => {
+        row.id = Number(row.id);
+        row.roadlinks.forEach((roadlink) => {
+          const feature = parse(
+            roadlink.f1.replace('LINESTRING ZM', 'LINESTRING')
+          );
+          if (feature?.type === 'LineString') {
+            const coordinates =
+              feature.coordinates as unknown as Array<LineString4D>;
+            const pointObjects: Array<LinkPoint> = coordinates.map(
+              (coordinate) => {
+                return {
+                  x: coordinate[0],
+                  y: coordinate[1],
+                  z: coordinate[2],
+                  m: coordinate[3]
+                };
+              }
+            );
+
+            roadlink.linkId = roadlink.f2;
+            roadlink.points = pointObjects;
+            delete roadlink.f1;
+            delete roadlink.f2;
+          }
+        });
+      });
+      client.end();
+      return res.rows;
+    })
+    .catch((e) => {
+      client.end();
+      console.log(e);
+      return;
+    });
 };
 
-export const main = middyfy(storeMunicipalityData);
+export const main = middyfy(gerNearbyLinks);
