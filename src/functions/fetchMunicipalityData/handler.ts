98213c3a88cdc5955cf99a9c9241783a20f5eb66
MarkusRegardh <markus.regardh@gmail.com>
2023-04-06 14:04:48 +0300

    Add fetching from parameterstore

--- src/functions/fetchMunicipalityData/handler.ts
+++ src/functions/fetchMunicipalityData/handler.ts
@@ -1,7 +1,26 @@
 import { middyfy } from '@libs/lambda';
+import { SSM, GetParameterCommand } from '@aws-sdk/client-ssm';
+
+const getParameter = async (name: string): Promise<string> => {
+  const ssm = new SSM({});
+  const getParametersCommand = new GetParameterCommand({
+    Name: name,
+    WithDecryption: true
+  });
+  const result = await ssm.send(getParametersCommand);
+  return result.Parameter.Value;
+};
 
 const fetchMunicipalityData = async (event) => {
-  console.log(event);
+  const municipality: string = event.municipality;
+  const apiKey = await getParameter(event.authentication);
+  const url = event.url;
+  const result = {
+    municipality: municipality,
+    apiKey: apiKey,
+    url: url
+  };
+  console.log(result);
 };
 
 export const main = middyfy(fetchMunicipalityData);
--- src/functions/fetchMunicipalityData/index.ts
+++ src/functions/fetchMunicipalityData/index.ts
@@ -1,18 +1,5 @@
 import { handlerPath } from '@libs/handler-resolver';
 
 export default {
-  handler: `${handlerPath(__dirname)}/handler.main`,
-  events: [
-    {
-      schedule: {
-        rate: ['cron(0 10 ? * TUE *)'],
-        input: {
-          municipality: 'espoo',
-          url: 'url.com',
-          user: 'kuntaUser',
-          password: 'password123'
-        }
-      }
-    }
-  ]
+  handler: `${handlerPath(__dirname)}/handler.main`
 };
