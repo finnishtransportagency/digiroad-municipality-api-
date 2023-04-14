0bef8ef1720f04e18583b6f2806d7ab208379627
MarkusRegardh <markus.regardh@gmail.com>
2023-04-14 20:42:22 +0300

    Fi typo in role

--- src/functions/deleteSchedule/index.ts
+++ src/functions/deleteSchedule/index.ts
@@ -2,7 +2,7 @@ import { handlerPath } from '@libs/handler-resolver';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
-  role: 'deleteSchedulesRole',
+  role: 'deleteScheduleRole',
   events: [
     {
       http: {
