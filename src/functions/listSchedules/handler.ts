2d6d695730495b7e4baaed6a5edaac0151e26324
MarkusRegardh <markus.regardh@gmail.com>
2023-04-14 18:55:52 +0300

    Add listSchedules

---
+++ src/functions/listSchedules/handler.ts
@@ -0,0 +1,31 @@
+import { middyfy } from '@libs/lambda';
+import {
+  SchedulerClient,
+  ListSchedulesCommand
+} from '@aws-sdk/client-scheduler';
+
+const createSchedule = async (event) => {
+  const scheduler = new SchedulerClient({});
+
+  const listScheduleInput = {
+    GroupName: `DRKunta-${process.env.STAGE_NAME}`
+  };
+
+  const listScheduleCommand = new ListSchedulesCommand(listScheduleInput);
+
+  try {
+    const response = await scheduler.send(listScheduleCommand);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 400,
+      message: 'Could not find schedules'
+    };
+  }
+};
+
+export const main = middyfy(createSchedule);
