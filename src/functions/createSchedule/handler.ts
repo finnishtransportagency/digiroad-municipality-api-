b97a8185808a574372d29a256f2396e3e26ba3a9
Henrik Sundell <henrik.sundell@gmail.com>
2023-04-25 13:17:10 +0300

    Add custom schedules
    Allow custom schedules as input

--- src/functions/createSchedule/handler.ts
+++ src/functions/createSchedule/handler.ts
@@ -21,6 +21,14 @@ const inputSchema = {
         key: { type: 'string', pattern: '^[a-zA-Z0-9]*$' },
         url: {
           type: 'string'
+        },
+        schedule: {
+          type: 'object',
+          properties: {
+            dayOfWeek: { type: 'integer', minimum: 0, maximum: 6 },
+            dayOfMonth: { type: 'integer', minimum: 1, maximum: 31 },
+            time: { type: 'integer', minimum: 0, maximum: 23 }
+          }
         }
       },
       required: ['municipality', 'key', 'url']
@@ -45,10 +53,34 @@ const createSchedule = async (event) => {
     Type: 'String'
   };
 
+  const toCron = (schedule) => {
+    let time: string;
+    if (schedule.dayOfWeek && schedule.time) {
+      time = `cron(0 ${schedule.time} ? * ${schedule.dayOfWeek} *)`;
+    } else if (schedule.dayOfMonth && schedule.time) {
+      time = `cron(0 ${schedule.time} ${schedule.dayOfMonth} * ? *)`;
+    } else {
+      console.error(
+        'Invalid schedule, time and dayOfWeek or dayOfMonth required'
+      );
+    }
+    return time;
+  };
+
+  const schedule = event.body.schedule
+    ? toCron(event.body.schedule)
+    : 'cron(0 10 ? * TUE *)';
+
+  if (!schedule) {
+    return {
+      statusCode: 400,
+      body: 'Invalid schedule, time and dayOfWeek/dayOfMonth are required'
+    };
+  }
   const createScheduleInput = {
     Name: `DRKunta-${process.env.STAGE_NAME}-${event.body.municipality}`,
     GroupName: `DRKunta-${process.env.STAGE_NAME}`,
-    ScheduleExpression: 'cron(0 10 ? * TUE *)',
+    ScheduleExpression: schedule,
     Target: {
       Arn: `arn:aws:lambda:eu-west-1:${process.env.AWS_ACCOUNT_ID}:function:DRKunta-${process.env.STAGE_NAME}-fetchMunicipalityData`,
       RoleArn: `arn:aws:iam::${process.env.AWS_ACCOUNT_ID}:role/DRKunta-${process.env.STAGE_NAME}-fetchMunicipalityDataScheduleRole`,
