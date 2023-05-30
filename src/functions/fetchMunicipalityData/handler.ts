4c50cdde11655d664fb8e3b21b66298c6eed1630
Henrik Sundell <henrik.sundell@gmail.com>
2023-05-30 11:22:32 +0300

    Add fetch from municipality api

--- src/functions/fetchMunicipalityData/handler.ts
+++ src/functions/fetchMunicipalityData/handler.ts
@@ -1,6 +1,10 @@
 import { middyfy } from '@libs/lambda';
 import { SSM, GetParameterCommand } from '@aws-sdk/client-ssm';
+import { S3 } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+import axios from 'axios';
 
+const s3 = new S3({});
 const getParameter = async (name: string): Promise<string> => {
   const ssm = new SSM({});
   const getParametersCommand = new GetParameterCommand({
@@ -12,20 +16,35 @@ const getParameter = async (name: string): Promise<string> => {
 };
 
 const fetchMunicipalityData = async (event) => {
-  const municipality: string = event.municipality;
   const apiKey = await getParameter(
     `/DRKunta/${process.env.STAGE_NAME}/${event.municipality}`
   );
-  const url = event.url;
-  const result = {
-    municipality: municipality,
-    apiKey: apiKey,
-    url: url,
-    format: event.format
-  };
 
-  //Request according to event details, result inserted to s3 bucket /infrao
-  console.log(result);
+  if (event.format === 'xml') {
+    const url =
+      event.url +
+      '/collections/infrao:Rakenne/items?f=gml&crs=http://www.opengis.net/def/crs/EPSG/0/3067';
+
+    const { data, status } = await axios.get(url, {
+      headers: {
+        'x-api-key': apiKey
+      }
+    });
+    console.log(status);
+    const now = new Date().toISOString().slice(0, 19);
+
+    const putParams = {
+      Bucket: `dr-kunta-${process.env.STAGE_NAME}-bucket`,
+      Key: `infrao/${event.municipality}/${now}.xml`,
+      Body: data
+    };
+
+    await new Upload({
+      client: s3,
+      params: putParams
+    }).done();
+  }
+  return;
 };
 
 export const main = middyfy(fetchMunicipalityData);
