ac9e8990b061bbba43a10b2c10dbc576c96e2c0e
MarkusRegardh <71496871+MarkusRegardh@users.noreply.github.com>
2023-05-30 14:10:10 +0300

    Refractor parseXML (#53)
    Refractor parseXML to match the API-data

--- src/functions/parseXML/datatypes/parseObstacles.ts
+++ src/functions/parseXML/datatypes/parseObstacles.ts
@@ -1,32 +1,28 @@
 import { Feature } from '@functions/typing';
 
-export default function (esterakennelmat) {
-  let features: Array<Feature> = [];
-  for (const esterakennelma of esterakennelmat) {
-    const feature = {
-      type: 'Feature',
-      properties: {
-        TYPE: 'OBSTACLE',
-        ID: esterakennelma['inf:yksilointitieto'],
-        EST_TYYPPI: 1 //esterakennelma['inf:rakenne']
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [
-          Number(
-            esterakennelma['inf:tarkkaSijaintitieto']['inf:Sijainti'][
-              'inf:piste'
-            ]['gml:Point']['gml:coordinates'].split(',')[0]
-          ),
-          Number(
-            esterakennelma['inf:tarkkaSijaintitieto']['inf:Sijainti'][
-              'inf:piste'
-            ]['gml:Point']['gml:coordinates'].split(',')[1]
-          )
-        ]
-      }
-    };
-    features = features.concat(feature);
-  }
-  return features;
+export default function (rakenne) {
+  const feature = {
+    type: 'Feature',
+    properties: {
+      TYPE: 'OBSTACLE',
+      ID: rakenne['yksilointitieto'],
+      EST_TYYPPI: 1 //esterakennelma.rakenne
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [
+        Number(
+          rakenne['tarkkaSijaintitieto']['Sijainti']['piste']['Point'][
+            'pos'
+          ].split(' ')[0]
+        ),
+        Number(
+          rakenne['tarkkaSijaintitieto']['Sijainti']['piste']['Point'][
+            'pos'
+          ].split(' ')[1]
+        )
+      ]
+    }
+  };
+  return feature;
 }
