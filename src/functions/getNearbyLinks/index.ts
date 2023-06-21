308f09077273d4bef9d42e31d45edc30d5c267a8
Henrik Sundell <henrik.sundell@gmail.com>
2023-06-21 14:58:59 +0300

    Increase timout for getNearbyLinks

--- src/functions/getNearbyLinks/index.ts
+++ src/functions/getNearbyLinks/index.ts
@@ -3,6 +3,7 @@ import { handlerPath } from '@libs/handler-resolver';
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   maximumRetryAttempts: 0,
+  timeout: 60,
   vpc: {
     securityGroupIds: [process.env.DIGIROADSECURITYGROUPID],
     subnetIds: [process.env.DIGIROADSUBNETAID, process.env.DIGIROADSUBNETBID]
