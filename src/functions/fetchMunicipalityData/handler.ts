887e5061ee6bcd79e6d693d44c32a2b2c70bbb3d
Eemeli Ingervo <eemeli.ingervo@cgi.com>
2024-03-05 15:52:46 +0200

    Better types: fetchMunicipalityData

--- src/functions/fetchMunicipalityData/handler.ts
+++ src/functions/fetchMunicipalityData/handler.ts
@@ -1,22 +1,25 @@
 import { middyfy } from '@libs/lambda';
 import { SSM, GetParameterCommand } from '@aws-sdk/client-ssm';
-import { S3 } from '@aws-sdk/client-s3';
+import { PutObjectCommandInput, S3 } from '@aws-sdk/client-s3';
 import { Upload } from '@aws-sdk/lib-storage';
 import axios from 'axios';
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
-import { offline, apikey } from '@functions/config';
+import { offline, apikey, testBbox } from '@functions/config';
+import { isScheduleEvent, isXmlFeatureCollectionJson } from './types';
 
-const s3config = offline
-  ? {
-      forcePathStyle: true,
-      credentials: {
-        accessKeyId: 'S3RVER', // This specific key is required when working offline
-        secretAccessKey: 'S3RVER'
-      },
-      endpoint: 'http://localhost:4569'
-    }
-  : {};
-const s3 = new S3(s3config);
+const s3 = new S3(
+  offline
+    ? {
+        forcePathStyle: true,
+        credentials: {
+          accessKeyId: 'S3RVER', // This specific key is required when working offline
+          secretAccessKey: 'S3RVER'
+        },
+        endpoint: 'http://localhost:4569'
+      }
+    : {}
+);
+
 const getParameter = async (name: string): Promise<string> => {
   const ssm = new SSM({});
   const getParametersCommand = new GetParameterCommand({
@@ -27,77 +30,89 @@ const getParameter = async (name: string): Promise<string> => {
   return result.Parameter.Value;
 };
 
-// SPECIFY TYPE & CREATE TYPE GUARDS ! ! ! ! ! ! ! ! !
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const mergeData = (dataArray: Array<any>) => {
+const mergeData = (dataArray: Array<string>): string => {
+  if (dataArray.length === 0) throw new Error('No data to merge');
+  if (dataArray.length === 1) return dataArray[0];
+
   const parser = new XMLParser();
-  dataArray.forEach((data) => parser.parse(data));
-  const result = parser.parse(dataArray[0]);
-  for (var i = 1; i < dataArray.length; i++) {
-    const chunk = parser.parse(dataArray[i]);
-    result['sf:FeatureCollection']['sf:featureMember'].push(
-      ...chunk['sf:FeatureCollection']['sf:featureMember']
-    );
-  }
+  const jsonArray = dataArray.map((data) => {
+    const parsed = parser.parse(data);
+    if (!isXmlFeatureCollectionJson(parsed)) {
+      throw new Error('Invalid XML data');
+    }
+    return parsed;
+  });
+
+  // Using the first xmlFeatureCollectionJson as a base and pushing the rest of the data into it
+  const result = jsonArray.slice(1).reduce(
+    (acc, curr) => {
+      acc['sf:FeatureCollection']['sf:featureMember'].push(
+        ...curr['sf:FeatureCollection']['sf:featureMember']
+      );
+      return acc;
+    },
+    { ...jsonArray[0] }
+  );
+
   const builder = new XMLBuilder({});
   const xmlResult = builder.build(result);
   return xmlResult;
 };
 
-const fetchMunicipalityData = async (event) => {
+const fetchMunicipalityData = async (event: unknown) => {
+  if (!isScheduleEvent(event)) {
+    throw new Error('Invalid event');
+  }
+
   const apiKey = offline
     ? apikey
     : await getParameter(
         `/DRKunta/${process.env.STAGE_NAME}/${event.municipality}`
       );
 
-  if (event.format === 'xml') {
-    for (var assetType of Object.entries(event.assetTypes)) {
-      var offset = 0;
-      const bbox = offline
-        ? '&bbox=379010,6677995,378228,6677525&bbox-crs=http://www.opengis.net/def/crs/EPSG/0/3067'
-        : '';
-      const dataArray = [];
-      var empty = false;
-      while (!empty) {
-        const url =
-          event.url +
-          `/collections/${
-            assetType[1]
-          }/items?f=gml&crs=http://www.opengis.net/def/crs/EPSG/0/3067&limit=5000&offset=${
-            offset * 5000
-          }${bbox}`;
+  for (var assetType of Object.entries(event.assetTypes)) {
+    const fetchSize = 5000;
+    let offset = 0;
+    const bbox = offline
+      ? `&bbox=${testBbox}&bbox-crs=http://www.opengis.net/def/crs/EPSG/0/3067`
+      : '';
+    const dataArray: Array<string> = [];
+    while (true) {
+      const url =
+        event.url +
+        `/collections/${
+          assetType[1]
+        }/items?f=gml&crs=http://www.opengis.net/def/crs/EPSG/0/3067&limit=${fetchSize}&offset=${
+          offset * fetchSize
+        }${bbox}`;
 
-        const { data } = await axios.get(url, {
-          headers: {
-            Authorization: apiKey
-          }
-        });
-        if (!data.includes('sf:featureMember')) {
-          empty = true;
-          break;
+      const { data } = await axios.get(url, {
+        headers: {
+          Authorization: apiKey
         }
-        dataArray.push(data);
-        offset += 1;
-      }
+      });
 
-      const payload = mergeData(dataArray);
+      if (typeof data !== 'string' || !data.includes('sf:featureMember')) break;
 
-      const now = new Date().toISOString().slice(0, 19);
-
-      const putParams = {
-        Bucket: `dr-kunta-${process.env.STAGE_NAME}-bucket`,
-        Key: `infrao/${event.municipality}/${assetType[0]}/${now}.xml`,
-        Body: payload
-      };
-
-      await new Upload({
-        client: s3,
-        params: putParams
-      }).done();
+      dataArray.push(data);
+      offset += 1;
     }
+
+    const payload = mergeData(dataArray);
+
+    const now = new Date().toISOString().slice(0, 19);
+
+    const putParams: PutObjectCommandInput = {
+      Bucket: `dr-kunta-${process.env.STAGE_NAME}-bucket`,
+      Key: `infrao/${event.municipality}/${assetType[0]}/${now}.xml`,
+      Body: payload
+    };
+
+    await new Upload({
+      client: s3,
+      params: putParams
+    }).done();
   }
-  return;
 };
 
 export const main = middyfy(fetchMunicipalityData);
--- src/functions/fetchMunicipalityData/index.ts
+++ src/functions/fetchMunicipalityData/index.ts
@@ -1,5 +1,6 @@
 import { handlerPath } from '@libs/handler-resolver';
 
+// This is the serverless configuration file for the fetchMunicipalityData function
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   maximumRetryAttempts: 0,
