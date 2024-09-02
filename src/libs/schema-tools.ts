991dffb11b4ca2d3d6260d0fe95718cb4f47f2c6
pe5 <eero.perola@gmail.com>
2024-09-02 14:56:26 +0300

    fixed obstacleschema bug

--- src/libs/schema-tools.ts
+++ src/libs/schema-tools.ts
@@ -6,14 +6,13 @@ import {
 import { array } from 'yup';
 
 export const arrayOfValidFeature = (assetTypeField: string) =>
-  array()
-    .when(assetTypeField, {
-      is: 'obstacles',
-      then: (schema) => schema.of(obstacleFeatureSchema),
-      otherwise: (schema) => schema
-    })
-    .when(assetTypeField, {
-      is: 'trafficSigns',
-      then: (schema) => schema.of(trafficSignFeatureSchema),
-      otherwise: (schema) => schema.of(roadSurfaceFeatureSchema)
-    });
+  array().when(assetTypeField, {
+    is: 'obstacles',
+    then: (schema) => schema.of(obstacleFeatureSchema),
+    otherwise: (schema) =>
+      schema.when(assetTypeField, {
+        is: 'trafficSigns',
+        then: (schema) => schema.of(trafficSignFeatureSchema),
+        otherwise: (schema) => schema.of(roadSurfaceFeatureSchema)
+      })
+  });
